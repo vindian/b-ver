0cc8f66ce2fb2ab43a873f61326fb1b8c56f9e02
vindian <vishire@gmail.com>
2022-08-30 09:48:50 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
 <H1>Version B</H1>
+<H3> Welcome to OpenShift Demo : 30th August 2022 </H3>
 <img src="b-abtesting.png">
 <div>
 <?php 
